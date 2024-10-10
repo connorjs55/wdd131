7b6814ce3dd8385b585cb9f61be26eaa41047e19
connorjs55 <connorjs55@gmail.com>
2024-10-09 21:39:14 -0600

    mission statement 2

---
+++ mission/mission.js
@@ -0,0 +1,17 @@
+const themeSelector = document.querySelector('#theme-select');
+
+function changeTheme() {
+    const body = document.body;
+    const logo = document.querySelector('#byui-logo');
+
+    if (themeSelector.value === 'dark') {
+        body.classList.add('dark');
+        logo.src = 'style/images/byui-logo_white.png';
+    
+    } else {
+        body.classList.remove('dark');
+        logo.src = 'style/images/byui-logo_blue.webp';
+    }
+}
+
+themeSelector.addEventListener('change', changeTheme);
